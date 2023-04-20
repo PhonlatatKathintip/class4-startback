e14b62a1dac904698a4dd2547c2671340fdafec3
Phonlatat.work <Phonlatues.w@gmail.com>
2023-04-20 23:34:26 +0700

    fail-lastpathroot

--- backend/index.js
+++ backend/index.js
@@ -2,17 +2,14 @@ const express = require("express");
 const app = express();
 const port = 3001;
 const User = require("./models/User");
+const router = express.Router();
+const userRouter = require("./routes/user.routes");
+module.exports = router;
 
-// const mockUser = [
-//   { id: 1, name: "Theethawat", department: "Software" },
-//   { id: 2, name: "Thanachit", department: "Software" },
-//   { id: 3, name: "Paipan", department: "Software" },
-//   { id: 4, name: "Aekapol", department: "Automation" },
-// ];
-
-// connectMongo //
 const mongoose = require("mongoose");
-const uri = "mongodb://127.0.0.1:27017/react-stater-test";
+const uri = "mongodb://127.0.0.1:27017/react-starter-test";
+
+// mongo
 mongoose.connect(uri).then(
   () => {
     console.log("Connection is Successful");
@@ -27,39 +24,4 @@ mongoose.connect(uri).then(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/user", async (req, res) => {
-  console.log("Create User Body", req.body);
-  const newUser = new User(req.body);
-  try {
-    await newUser.save({});
-    res.status(201).json(newUser);
-  } catch (error) {
-    res.status(400).json({ err: error });
-  }
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
-
-//function//
-// app.get("/user", async (req, res) => {
-//   try {
-//     const result = await User.find();
-//     console.log("Find All Users", result);
-//     res.json({ rows: result });
-//   } catch (error) {
-//     console.log("error All Users", error);
-//     res.status(404).json({ err: error });
-//   }
-// });
-
-app.get("/user/:id", (req, res) => {
-  console.log("Fine One User with Id" + req.params.id);
-  const foundUser = result.find((data) => data.id === parseInt(req.params.id));
-  res.json(foundUser);
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+app.use("/user", userRouter);
---
+++ backend/routes.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+
+router.get("/", async (req, res) => {
+  console.log("Find All Users");
+  try {
+    const result = await User.find();
+    res.json({ rows: result });
+  } catch (error) {
+    res.status(404).json({ err: error });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  console.log("Find All Users");
+  try {
+    const result = await User.findById(req?.params?.id);
+    res.json(result);
+  } catch (error) {
+    res.status(404).json({ err: error });
+  }
+});
+
+router.post("/", async (req, res) => {
+  console.log("Create User Body", req.body);
+  const newUser = new User(req.body);
+  try {
+    await newUser.save({});
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).json({ err: error });
+  }
+});
+
+module.exports = router;
